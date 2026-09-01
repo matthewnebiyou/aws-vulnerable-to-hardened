c24eb7ad1caa0d1a62e0d9912aef694c8d446760
Matthew Nebiyou <matthewnebiyou@gmail.com>
2026-09-01 14:15:07 -0400

    Copied the Terraform infrastructure to hardened/ as a starting ground for remediation of those initial five planned vulnerabilities

---
+++ environments/hardened/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy into: US East 1"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name prefix used for tagging resources"
+  type        = string
+  default     = "aws-vuln-to-hard"
+}
+
+variable "my_ip" {
+  description = "Public IP in CIDR form. Not used to restrict anything here, since this environment is intentionally open."
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
